c466dcde8d9ea595540bb47737d64eab8a757c19
Nitesh Devnath <niteshubi19@gmail.com>
2020-11-05 21:45:52 +0530

    new changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.dashboard');
-});
 
 Auth::routes();
+Route::group(['middleware' => ['auth']], function(){
+	Route::get('/', function () {
+    	return view('admin.dashboard');
+	});
+	Route::resource('categories', CategoryController::class);
+	Route::resource('users', UserController::class);
+	Route::get('/home', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('home');
+});
+
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/home', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('home');
